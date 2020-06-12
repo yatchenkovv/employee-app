035401e187175f43affb98a71667c60120422912
yatchenkovv <yatchenko.1990@mail.ru>
2020-06-12 15:37:30 +0500

    Updates

---
+++ precache-manifest.296849ef613a84346d72044a0c8f0c5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c877f71eb9105fdf62f182fa608339fe",
+    "url": "/employee-app/index.html"
+  },
+  {
+    "revision": "35df51d4215a75f3894c",
+    "url": "/employee-app/static/css/2.d53c156d.chunk.css"
+  },
+  {
+    "revision": "e7dc7a2bc0299249c3d8",
+    "url": "/employee-app/static/css/main.973a5d53.chunk.css"
+  },
+  {
+    "revision": "35df51d4215a75f3894c",
+    "url": "/employee-app/static/js/2.95090518.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/employee-app/static/js/2.95090518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7dc7a2bc0299249c3d8",
+    "url": "/employee-app/static/js/main.9ef62fce.chunk.js"
+  },
+  {
+    "revision": "93a1a245ea8ffce0464b",
+    "url": "/employee-app/static/js/runtime-main.1c9fe229.js"
+  }
+]);
